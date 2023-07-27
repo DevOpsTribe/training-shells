41cba66a4f273e222dea3f2536f7cffbc59f0ba1
Eugenio Marzo <eugenio.marzo@sourcesense.com>
2023-07-27 21:49:27 +0200

    test

--- ansible-training-lab/roles/devopstribe-training-lab/files/firewall-enable.sh
+++ ansible-training-lab/roles/devopstribe-training-lab/files/firewall-enable.sh
@@ -16,9 +16,16 @@ iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
 iptables -A OUTPUT -p tcp -d deb.debian.org -j ACCEPT
 iptables -A OUTPUT -p tcp -d github.com -j ACCEPT
 iptables -A OUTPUT -p tcp -d docker.io -j ACCEPT
+iptables -A OUTPUT -p tcp -d 140.82.121.4 -j ACCEPT
+iptables -A OUTPUT -p tcp -d  205.251.193.165 -j ACCEPT
+iptables -A OUTPUT -p tcp -d  198.51.44.8 -j ACCEPT
+iptables -A OUTPUT -p tcp -d  198.51.45.8 -j ACCEPT
+iptables -A OUTPUT -p tcp -d  198.51.44.72 -j ACCEPT
+iptables -A OUTPUT -p tcp -d  198.51.45.72 -j ACCEPT
+iptables -A OUTPUT -p tcp -d  140.82.121.3 -j ACCEPT
+iptables -A OUTPUT -p tcp -d  185.199.108.133 -j ACCEPT
 
 iptables -A INPUT -s 10.10.10.0/24 -p tcp --dport 22 -m state --state NEW -j ACCEPT
-
 
 # Default policy to drop 'everything' but our output to internet
 iptables -P FORWARD DROP
